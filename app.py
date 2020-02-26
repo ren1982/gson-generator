be35ec5167d4d9495319af7ad51fa4d32389fa61
Rainier Robles <renrobles@gmail.com>
2020-02-26 17:12:45 +0100

    postgres

--- app.py
+++ app.py
@@ -1,39 +1,50 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 # from pymarkovchain import MarkovChain
 import requests
-# from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 import markovify
-
+import os
 
 app = Flask(__name__)
-# db = SQLAlchemy()
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL'] #"postgresql://gson:gson@localhost/gson"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 # db.init_app(app)
+
+from .models import Lyrics
 
 
 @app.route('/')
 def main():
     return render_template('gson_generator/main.html')
 
+
 @app.route('/generate', methods=['POST'])
 def generate_lyrics():
     lyrics_location = url_for('static', filename='lyrics.json', _external=True)
     response = requests.get(lyrics_location)
     lyric_list = response.json()
-    lyrics = ''
+    lyric_text = ''
     for lyric in lyric_list:
-        lyrics += lyric['lyrics'].replace('...', '') + ' '
-    # mc = MarkovChain()
-    # mc.generateDatabase(lyrics)
-    # generated_lyrics = []
-    # for line in range(20):
-    #     generated_lyrics.append(mc.generateString())
-    text_model = markovify.NewlineText(lyrics, state_size=1)
+        lyric_text += lyric['lyrics'].replace('...', '') + ' '
+    text_model = markovify.NewlineText(lyric_text, state_size=1)
     generated_lyrics = []
     title = text_model.make_sentence(tries=100, max_overlap_ratio=5)
     for i in range(24):
         generated_lyrics.append(text_model.make_sentence(tries=100, max_overlap_ratio=5))
-    return render_template('gson_generator/lyrics.html', title=title, lyrics=generated_lyrics)
+    lyrics = Lyrics(title=title, lyrics=generated_lyrics)
+    db.session.add(lyrics)
+    db.session.commit()
+    url = lyrics.url
+    return redirect(url_for('display_lyrics', url=url))
 
+
+@app.route('/<url>')
+def display_lyrics(url):
+    lyrics = Lyrics.query.filter_by(url=url).first()
+    title = lyrics.title
+    lyrics = lyrics.lyrics
+    return render_template('gson_generator/lyrics.html', title=title, lyrics=lyrics, url=url)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,23 +1,25 @@
-# from datetime import datetime
-# import secrets
-#
-# from app import db
-#
-#
-# class Lyrics(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     lyrics = db.Column(db.Text, nullable=False)
-#     url = db.Column(db.String(10), unique=True)
-#     visits = db.Column(db.Integer, default=0)
-#     date_created = db.Column(db.DateTime, default=datetime.now)
-#
-#     def __init__(self, **kwargs):
-#         super().__init__(**kwargs)
-#         self.url = self.generate_url()
-#
-#     def generate_url(self):
-#         url = secrets.token_hex(10)
-#         item = self.query.filter_by(url=url).first()
-#         if item:
-#             return self.generate_url()
-#         return url
+from datetime import datetime
+from sqlalchemy.types import ARRAY
+import secrets
+
+from .app import db
+
+
+class Lyrics(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    lyrics = db.Column(ARRAY(db.Text(length=250)), nullable=False)
+    url = db.Column(db.String(10), unique=True)
+    visits = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.now)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.url = self.generate_url()
+
+    def generate_url(self):
+        url = secrets.token_hex(5)
+        item = self.query.filter_by(url=url).first()
+        if item:
+            return self.generate_url()
+        return url
