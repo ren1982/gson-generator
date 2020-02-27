6e04a05e8357bab4216be50638e8e55a07252e12
Rainier Robles <renrobles@gmail.com>
2020-02-27 11:20:57 +0100

    added features as well as bootstrap

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request
 # from pymarkovchain import MarkovChain
 import requests
 from flask_sqlalchemy import SQLAlchemy
@@ -11,7 +11,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 # db.init_app(app)
 
-from models import Lyrics
+from .models import Lyrics
 
 
 @app.route('/')
@@ -29,9 +29,9 @@ def generate_lyrics():
         lyric_text += lyric['lyrics'].replace('...', '') + ' '
     text_model = markovify.NewlineText(lyric_text, state_size=1)
     generated_lyrics = []
-    title = text_model.make_sentence(tries=100, max_overlap_ratio=5)
+    title = text_model.make_sentence(tries=100, max_words=7)
     for i in range(24):
-        generated_lyrics.append(text_model.make_sentence(tries=100, max_overlap_ratio=5))
+        generated_lyrics.append(text_model.make_sentence(tries=100, max_overlap_ratio=0.6))
     lyrics = Lyrics(title=title, lyrics=generated_lyrics)
     db.session.add(lyrics)
     db.session.commit()
@@ -41,10 +41,25 @@ def generate_lyrics():
 
 @app.route('/<url>')
 def display_lyrics(url):
-    lyrics = Lyrics.query.filter_by(url=url).first()
-    title = lyrics.title
-    lyrics = lyrics.lyrics
-    return render_template('gson_generator/lyrics.html', title=title, lyrics=lyrics, url=url)
+    lyrics_ = Lyrics.query.filter_by(url=url).first()
+    if lyrics_:
+        lyrics_.visits = lyrics_.visits + 1
+        db.session.commit()
+        return render_template('gson_generator/lyrics.html', lyrics=lyrics_)
+    return redirect(url_for('main'))
+
+
+@app.route('/explore')
+@app.route('/explore/<order>')
+def list_generated_lyrics(order='recent'):
+    page = request.args.get('page', 1, type=int)
+    if order == 'recent':
+        listing = Lyrics.query.order_by(Lyrics.date_created.desc()).paginate(page=page, per_page=10)
+    elif order == 'popular':
+        listing = Lyrics.query.order_by(Lyrics.visits.desc()).paginate(page=page, per_page=10)
+    else:
+        return redirect(url_for('main'))
+    return render_template('gson_generator/list.html', listing=listing, order=order)
 
 
 if __name__ == '__main__':
